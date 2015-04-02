db5379a127f591310ae231307084b36609fa94c4
jesusdario <jesusdriru@gmail.com>
2015-04-02 10:27:56 +0200

    Adds cli tools

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-# xway dashboard
+## xway dashboard
 
 Iot in html5.
 
 This is a web panel from you can install, manage or publish IoT applications. Multiplatform. Any device supporting once (Chromium V8 Javascript runtime) can deploy it.
 
-``` Further
-
+### Install
+``` bash
+npm install -g xy-dashboard
 ```
 
-documentation coming soon.```
+### Run
+```bash
+# Port is optional, it will run by default on port 3000
+xy-dasyboard --port 8080
+```
+
+### Docs
+http://docs.netbeast.co
+
+#### How to build your own xway with Raspberry Pi
+https://medium.com/@netbeast/build-your-own-xway-e626d68977a8
--- src/main.js
+++ src/main.js
@@ -15,10 +15,6 @@ var activitiesRoutes = require('./routes/activities');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, '..', 'public', 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -48,10 +44,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.status(500).json("Server Error");
   });
 }
 
@@ -59,10 +52,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.status(500).json("Server Error");
 });
 
 module.exports = app;

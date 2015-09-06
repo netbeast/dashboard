172e5ea1394f6b03b225cb413a388b721d284206
jesusdario <jesusdriru@gmail.com>
2015-09-06 18:14:27 +0200

    Adds deamon to Da project

---
+++ bin/deamon.js
@@ -0,0 +1,28 @@
+#!/bin/sh
+':' // exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+var sha1 = require('sha1')
+var dgram = require('dgram')
+var deamon = dgram.createSocket("udp4")
+
+// aurum number port
+deamon.bind(16180, function() {
+	deamon.addMembership('239.0.16.18')
+})
+
+deamon.on("listening", function () {
+	var addr = deamon.address()
+	console.log("deamon ready at %s:%s", addr.address, addr.port)
+})
+
+deamon.on("message", function (msg, req) {
+	console.log("%s from %s:%s", msg, req.address, req.port)
+	// answer from the interface we got the request
+	var message = new Buffer(sha1('16180'))
+	deamon.send(message, 0, message.length, req.port, req.address, 
+		function(err, bytes) {
+			if (err) 
+				throw err
+			console.log('UDP message sent to ' + req.address +':'+ req.port)
+		})
+})

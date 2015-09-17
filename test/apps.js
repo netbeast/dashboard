640f8aba3bf92dbb366a1f05533cd12acfe3d0a4
jesusdario <jesusdriru@gmail.com>
2015-09-17 19:14:17 +0200

    up tests/apps for travis-ci testing

--- test/apps.js
+++ test/apps.js
@@ -28,38 +28,16 @@ describe("Apps", function(){
 		setTimeout(done, 2000)
 	})
 
-	describe("#GET", function(){
-
-		it("should return all apps names", function(done) {
-			request('http://localhost:3000/apps', function (err, resp, body) {
-				should.not.exist(err)
-				resp.statusCode.should.equal(200)
-				expect(JSON.parse(body)).to.be.an('Array')
-				done()
-			})
-		})
-
-		it("should return application package.json", function(done) {
-			request('http://localhost:3000/apps/Market', function (err, resp, body) {
-				should.not.exist(err)
-				resp.statusCode.should.equal(200)
-				expect(JSON.parse(body)).to.be.an('Object')
-				done()
-			})
-		})
-
-		it("should return 404 when an app does not exist", function(done) {
-			request('http://localhost:3000/apps/tsaebten', function (err, resp, body) {
-				should.not.exist(err)
-				expect(resp.statusCode).to.equal(404)
-				body.should.be.a('String')
-				done()
-			})
+	it("should return 404 when an app does not exist", function(done) {
+		request('http://localhost:3000/apps/tsaebten', function (err, resp, body) {
+			should.not.exist(err)
+			expect(resp.statusCode).to.equal(404)
+			body.should.be.a('String')
+			done()
 		})
 	})
 
-	describe("#POST", function() {
-		it("should upload myapp to repository", function(done) {
+	it("should upload myapp to repository", function(done) {
 			this.timeout(8*s) //this takes time
 			var req = request.post('http://localhost:3000/apps', function (err, resp, body) {
 				should.not.exist(err)
@@ -76,55 +54,68 @@ describe("Apps", function(){
 			form.append('file', fs.createReadStream(app))
 		})
 
-		it("should upload app @ github to repo", function (done) {
-			this.timeout(20*s) //this also takes time
-			var req = request.post('http://localhost:3000/apps', function (err, resp, body) {
-				should.not.exist(err)
-				body.should.equal('')
-				expect(resp.statusCode).to.equal(204)
-				fs.stat(path.join(config.appsDir, 'xy-get-started'), function (err, stats) {
-					should.not.exist(err)
-					expect(stats.isDirectory()).to.equal(true)
-					done()
-				})
-			})
-			var form = req.form()
-			var app = 'https://github.com/netbeast-co/get-started'
-			form.append('url', app)
-		})
-
-	})
-	//
-	describe("#DELETE ", function() {
-		
-		it("should remove 'myapp' from repository", function(done) {
-			request.del('http://localhost:3000/apps/myapp', function (err, resp, body) {
-				should.not.exist(err)
-				expect(resp.statusCode).to.equal(204)
-				body.should.equal('')
-				done()
-			})
-		})
-
-		it("should remove 'xy-get-started' from repository", function(done) {
-			request.del('http://localhost:3000/apps/xy-get-started', function (err, resp, body) {
-				should.not.exist(err)
-				expect(resp.statusCode).to.equal(204)
-				body.should.equal('')
-				done()
-			})
-		})
-
-		it("should return 404 when an app does not exist", function(done) {
-			request.del('http://localhost:3000/apps/tsaebten', function (err, resp, body) {
-				should.not.exist(err)
-				expect(resp.statusCode).to.equal(404)
-				body.should.be.a('String')
-				done()
-			})
+	it("should return application package.json", function(done) {
+		request('http://localhost:3000/apps/myapp', function (err, resp, body) {
+			should.not.exist(err)
+			resp.statusCode.should.equal(200)
+			expect(JSON.parse(body)).to.be.an('Object')
+			done()
 		})
 	})
 
+	it("should upload app @ github to repo", function (done) {
+		this.timeout(20*s) //this also takes time
+		var req = request.post('http://localhost:3000/apps', function (err, resp, body) {
+			should.not.exist(err)
+			body.should.equal('')
+			expect(resp.statusCode).to.equal(204)
+			fs.stat(path.join(config.appsDir, 'xy-get-started'), function (err, stats) {
+				should.not.exist(err)
+				expect(stats.isDirectory()).to.equal(true)
+				done()
+			})
+		})
+		var form = req.form()
+		var app = 'https://github.com/netbeast-co/get-started'
+		form.append('url', app)
+	})
+
+	it("should return all apps names", function(done) {
+		request('http://localhost:3000/apps', function (err, resp, body) {
+			should.not.exist(err)
+			resp.statusCode.should.equal(200)
+			expect(JSON.parse(body)).to.be.an('Array')
+			done()
+		})
+	})
+
+	it("should remove 'myapp' from repository", function(done) {
+		request.del('http://localhost:3000/apps/myapp', function (err, resp, body) {
+			should.not.exist(err)
+			expect(resp.statusCode).to.equal(204)
+			body.should.equal('')
+			done()
+		})
+	})
+
+	it("should remove 'xy-get-started' from repository", function(done) {
+		request.del('http://localhost:3000/apps/xy-get-started', function (err, resp, body) {
+			should.not.exist(err)
+			expect(resp.statusCode).to.equal(204)
+			body.should.equal('')
+			done()
+		})
+	})
+
+	it("should return 404 when an app does not exist", function(done) {
+		request.del('http://localhost:3000/apps/tsaebten', function (err, resp, body) {
+			should.not.exist(err)
+			expect(resp.statusCode).to.equal(404)
+			body.should.be.a('String')
+			done()
+		})
+	})
+	
 	after(function() {
 		dashboard.kill()
 	})

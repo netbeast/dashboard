cf53ae067da2488bde33ab5b193d5ade2a544e1b
jesusdario <jesusdriru@gmail.com>
2015-09-17 11:05:34 +0200

    updates test and travis-ci

--- test/apps.js
+++ test/apps.js
@@ -5,6 +5,7 @@ var chai = require('chai')
 , expect = chai.expect
 
 var request = require('request')
+, spawn = require('child_process').spawn
 , config = require('../config')
 , path = require('path')
 , fs = require('fs')
@@ -18,10 +19,18 @@ var s = 1000 // seconds
 // http://code.tutsplus.com/tutorials/testing-in-nodejs--net-35018
 
 describe("Apps", function(){
+
+	var dashboard = undefined
+
+	before(function(done) {
+		dashboard = spawn('./www', ['-p', '3000'], {})
+		setTimeout(done, 1000)
+	})
+
 	describe("#GET", function(){
 
 		it("should return all apps names", function(done) {
-			request('http://localhost/apps', function (err, resp, body) {
+			request('http://localhost:3000/apps', function (err, resp, body) {
 				should.not.exist(err)
 				resp.statusCode.should.equal(200)
 				expect(JSON.parse(body)).to.be.an('Array')
@@ -30,7 +39,7 @@ describe("Apps", function(){
 		})
 
 		it("should return application package.json", function(done) {
-			request('http://localhost/apps/Market', function (err, resp, body) {
+			request('http://localhost:3000/apps/Market', function (err, resp, body) {
 				should.not.exist(err)
 				resp.statusCode.should.equal(200)
 				expect(JSON.parse(body)).to.be.an('Object')
@@ -39,7 +48,7 @@ describe("Apps", function(){
 		})
 
 		it("should return 404 when an app does not exist", function(done) {
-			request('http://localhost/apps/tsaebten', function (err, resp, body) {
+			request('http://localhost:3000/apps/tsaebten', function (err, resp, body) {
 				should.not.exist(err)
 				expect(resp.statusCode).to.equal(404)
 				body.should.be.a('String')
@@ -51,7 +60,7 @@ describe("Apps", function(){
 	describe("#POST", function() {
 		it("should upload myapp to repository", function(done) {
 			this.timeout(3*s) //this takes time
-			var req = request.post('http://localhost/apps', function (err, resp, body) {
+			var req = request.post('http://localhost:3000/apps', function (err, resp, body) {
 				should.not.exist(err)
 				body.should.equal('')
 				expect(resp.statusCode).to.equal(204)
@@ -68,7 +77,7 @@ describe("Apps", function(){
 
 		it("should upload app @ github to repo", function (done) {
 			this.timeout(100*s) //this also takes time
-			var req = request.post('http://localhost/apps', function (err, resp, body) {
+			var req = request.post('http://localhost:3000/apps', function (err, resp, body) {
 				should.not.exist(err)
 				body.should.equal('')
 				expect(resp.statusCode).to.equal(204)
@@ -88,7 +97,7 @@ describe("Apps", function(){
 	describe("#DELETE ", function() {
 		
 		it("should remove 'myapp' from repository", function(done) {
-			request.del('http://localhost/apps/myapp', function (err, resp, body) {
+			request.del('http://localhost:3000/apps/myapp', function (err, resp, body) {
 				should.not.exist(err)
 				expect(resp.statusCode).to.equal(204)
 				body.should.equal('')
@@ -97,7 +106,7 @@ describe("Apps", function(){
 		})
 
 		it("should remove 'xy-get-started' from repository", function(done) {
-			request.del('http://localhost/apps/xy-get-started', function (err, resp, body) {
+			request.del('http://localhost:3000/apps/xy-get-started', function (err, resp, body) {
 				should.not.exist(err)
 				expect(resp.statusCode).to.equal(204)
 				body.should.equal('')
@@ -106,13 +115,17 @@ describe("Apps", function(){
 		})
 
 		it("should return 404 when an app does not exist", function(done) {
-			request.del('http://localhost/apps/tsaebten', function (err, resp, body) {
+			request.del('http://localhost:3000/apps/tsaebten', function (err, resp, body) {
 				should.not.exist(err)
 				expect(resp.statusCode).to.equal(404)
 				body.should.be.a('String')
 				done()
 			})
 		})
+	})
+
+	after(function() {
+		dashboard.kill()
 	})
 
 })

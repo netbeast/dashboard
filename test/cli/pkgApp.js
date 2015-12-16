16a26e6d177ca1f43a9f501ede736b375e0a02db
fcojriosbello <fcojriosbello@MacBook-Pro-de-Francisco.local>
2015-12-16 21:27:17 +0100

    Pkg tests finished

--- test/cli/pkgApp.js
+++ test/cli/pkgApp.js
@@ -1,7 +1,6 @@
-/* global describe, before, after, it*/
+/* global describe, before, it*/
 
 var should = require('chai').should()
-// var expect = require('chai').expect
 
 var exec = require('child_process').exec
 var fs = require('fs')
@@ -15,40 +14,46 @@ describe('Command Line Interface: package', function () {
   })
   it('should create a packaged app from testApp called app.tar.gz', function (done) {
     var pathToPkg = process.cwd()
-    fs.readFile(pathToPkg + '/app.tar.gz', function (err, file) {
+    fs.access(pathToPkg + '/app.tar.gz', fs.F_OK, function (err) {
       should.not.exist(err)
-      should.exist(file)
-      done()
+      exec('rm -rf app.tar.gz', function (err, stdout, stderr) {
+        should.not.exist(err)
+        done()
+      })
     })
   })
-  // package app with optoion -o
-  after('it should create a packaged app from testApp called testApp.tar.gz (option -o)', function (done) {
-    exec('beast package testApp -o testApp.tar.gz', function (err, stdout, stderr) {
+  // package app with option -o
+  before(function (done) {
+    exec('beast package testApp -o testAppO.tar.gz', function (err, stdout, stderr) {
       should.not.exist(err)
       done()
     })
   })
   it('should create a packaged app from testApp called testApp.tar.gz (option -o)', function (done) {
     var pathToPkg = process.cwd()
-    fs.readFile(pathToPkg + '/testApp.tar.gz', function (err, file) {
+    fs.access(pathToPkg + '/testAppO.tar.gz', fs.F_OK, function (err) {
       should.not.exist(err)
-      should.exist(file)
-      done()
+      exec('rm -rf testAppO.tar.gz', function (err, stdout, stderr) {
+        should.not.exist(err)
+        done()
+      })
     })
   })
-
-  after('it should create a packaged app from testApp called testApp.tar.gz (option --to)', function (done) {
-    exec('beast package testApp --to testApp.tar.gz', function (err, stdout, stderr) {
+  // package app with option --to
+  before(function (done) {
+    exec('beast package testApp -o testAppTO.tar.gz', function (err, stdout, stderr) {
       should.not.exist(err)
       done()
     })
   })
   it('should create a packaged app from testApp called testApp.tar.gz (option --to)', function (done) {
     var pathToPkg = process.cwd()
-    fs.readFile(pathToPkg + '/testApp.tar.gz', function (err, file) {
+    fs.access(pathToPkg + '/testAppTO.tar.gz', fs.F_OK, function (err) {
       should.not.exist(err)
-      should.exist(file)
-      done()
+      exec('rm -rf testAppTO.tar.gz testApp', function (err, stdout, stderr) {
+        should.not.exist(err)
+        done()
+      })
     })
   })
 })

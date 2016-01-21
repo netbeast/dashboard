d653a0ad7cc47c969ad3954a2cd01ec1850cb489
pablopi <pablopizarroaguilar@gmail.com>
2016-01-21 18:06:49 +0100

    Change table actions to resources and quit enveloper

--- src/routes/resource.js
+++ src/routes/resource.js
@@ -12,9 +12,9 @@ router.route('/resources')
 
 .get(function (req, res, next) {
   Resource.find(req.query, function (err, resources) {
-    if (err) return next(err)
+    if (err) return res.status(500).send(err)
 
-    res.json({ error: {}, data: resources })
+    res.json(resources)
   })
 })
 
@@ -22,37 +22,35 @@ router.route('/resources')
   Resource.findOne(req.body, function (err, resource) {
     if (!resource || err === 'No Row Found!') {
       Resource.create(req.body, function (err, item) {
-        if (err) res.status(500).send({ error: err, data: {} })
-        else res.status(204).end()
+        if (err) return res.status(500).send(err)
+        return res.status(204).end()
       })
-    } else if (err && err !== 'No Row Found!') res.status(500).send({ error: err, data: {} })
-    else res.status(500).send({ error: 'This action exists!', data: {} })
+    } else if (err && err !== 'No Row Found!') return res.status(500).send(err)
+    return res.status(500).send('This action exists!')
   })
 })
 
 .patch(function (req, res, next) {
   Resource.findOne(req.query, function (err, resource) {
-    if (err) res.status(500).send({ error: err, data: {} })
-    else {
-      Resource.update(req.query, req.body, function (err) {
-        if (err) res.status(500).send({ error: err, data: {} })
-        else res.status(204).end()
-      })
-    }
+    if (err) return res.status(500).send(err)
+
+    Resource.update(req.query, req.body, function (err) {
+      if (err) return res.status(500).send(err)
+      return res.status(204).end()
+    })
   })
 })
 
 .delete(function (req, res, next) {
   Resource.find(req.query, function (err, resources) {
-    if (err) res.status(500).send({ error: err, data: {} })
-    else {
-      resources.forEach(function (item) {
-        item.destroy(function (err) {
-          if (err) res.status(500).send({ error: err, data: {} })
-          else res.status(204).end()
-        })
+    if (err) return res.status(500).send(err)
+
+    resources.forEach(function (item) {
+      item.destroy(function (err) {
+        if (err) return res.status(500).send(err)
+        return res.status(204).end()
       })
-    }
+    })
   })
 })
 

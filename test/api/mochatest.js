e98a2c3ddf92f1e769c8a6e805d78e1adeea9c83
Htmael <htmael@gmail.com>
2016-02-24 11:32:29 +0100

    New test Method, Find Method

--- test/api/mochatest.js
+++ test/api/mochatest.js
@@ -3,57 +3,63 @@ var expect = require('chai').expect
 var netbeast = require('netbeast')
 var mqtt = require('mqtt')
 
-describe('error', function (body, title) {
-  it('notification of error to dashboard', function () {
-    expect(netbeast().error(body, title)).to.satisfy(function () {
-      var client = mqtt.connect('ws://NETBEAST_IP:PORT')
-      client.subscribe('netbeast/push')
-      client.on('message', function (message) {
-        if (message.emphasis === 'error') {
-          if (message.body === body) {
-            if (message.title === title) {
-              console.log('hola')
-            }
-          }
-        }
-      })
-    })
+describe('MQTT methods', function () {
+
+  var body = 'body'
+  var title = 'title'
+
+it('send error notification to dashboard', function (done) {
+
+  var client = mqtt.connect('ws://' + process.env.NETBEAST)
+
+  client.subscribe('netbeast/push')
+  client.on('message', function (message) {
+    if (message.emphasis === 'error' && message.body === body && message.title === title) {
+      done()
+    }
+    netbeast().error(body, title)
+  })
+}),
+
+it('send info notification to dashboard', function (done) {
+  var client = mqtt.connect('ws://' + process.env.NETBEAST)
+  client.subscribe('netbeast/push')
+  client.on('message', function (message) {
+    if (message.emphasis === 'error' && message.body === body && message.title === title) {
+      done()
+    }
+    netbeast().error(body, title)
+  })
+}),
+
+it('send success notification to dashboard', function (done) {
+  var client = mqtt.connect('ws://' + process.env.NETBEAST)
+  client.subscribe('netbeast/push')
+  client.on('message', function (message) {
+    if (message.emphasis === 'error' && message.body === body && message.title === title) {
+      done()
+    }
+    netbeast().error(body, title)
+  })
+}),
+
+it('send warning notification to dashboard', function (done) {
+  var client = mqtt.connect('ws://' + process.env.NETBEAST)
+  client.subscribe('netbeast/push')
+  client.on('message', function (message) {
+    if (message.emphasis === 'error' && message.body === body && message.title === title) {
+      done()
+    }
+    netbeast().error(body, title)
   })
 })
-
-describe('warning', function (body, title) {
-  it('notification of warning to dashboard', function () {
-    expect(netbeast().error(body, title)).to.satisfy(function () {
-      var client = mqtt.connect('ws://NETBEAST_IP:PORT')
-      client.subscribe('netbeast/push')
-      client.on('message', function (message) {
-        if (message.emphasis === 'warning') {
-          if (message.body === body) {
-            if (message.title === title) {
-              console.log('hola')
-            }
-          }
-        }
-      })
-    })
-  })
 })
 
-
-describe('info', function (body, title) {
-  it('notification of info to dashboard', function () {
-    expect(netbeast().error(body, title)).to.satisfy(function () {
-      var client = mqtt.connect('ws://NETBEAST_IP:PORT')
-      client.subscribe('netbeast/push')
-      client.on('message', function (message) {
-        if (message.emphasis === 'info') {
-          if (message.body === body) {
-            if (message.title === title) {
-              console.log('hola')
-            }
-          }
-        }
-      })
-    })
+describe('Find Method', function () {
+  it('return the IP address and the port', function (done) {
+    expect(netbeast().find()).to.have.all.keys(
+      'adress', 'port'
+    )
+    done()
   })
 })

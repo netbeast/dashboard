ace7c5402b2b95f78c82699c655a48960765b459
jsdario <jesus@netbeast.co>
2016-02-29 17:40:17 +0100

    Improves explore view separating plugins from apps.

---
+++ public/components/apps/explorable-app.jsx
@@ -0,0 +1,80 @@
+/* global toastr */
+import React from 'react'
+import request from 'superagent-bluebird-promise'
+
+export default class ExplorableApp extends React.Component {
+  constructor (props, context) {
+    super(props)
+    this.router = context.router
+    this.state = { hidden: false }
+  }
+
+  componentDidMount () {
+    const GITHUB_Q = 'https://raw.githubusercontent.com/' + this.props.full_name + '/master/package.json'
+
+    request.get(GITHUB_Q).end((err, res) => {
+      if (err) return this.setState({ hidden: true })
+
+      const packageJson = JSON.parse(res.text)
+      this.setState({ netbeast: packageJson.netbeast, logo: packageJson.logo })
+    })
+  }
+
+  launch () {
+    const { name } = this.props
+
+    request.post('/api/activities/' + name).then(() => {
+      return request.get('/i/' + name).promise()
+    }).then(() => {
+      this.router.push('/i/' + name)
+    }).catch((err) => {
+      if (err.status === 404) return toastr.info(`${name} is running`)
+      toastr.error(err.message)
+    })
+  }
+
+  install () {
+    const url = this.props.git_url
+
+    request.post('/api/apps').send({ url }).then((res) => {
+      const name = res.body.name
+      const props = res.body.netbeast
+      const type = props ? props.type : 'app'
+
+      toastr.success(`${name} has been installed!`)
+
+      if (type === 'plugin' || type === 'service' || props.bootOnLoad)
+        return request.post('/api/activities/' + name).promise()
+    }).then((res) => { toastr.success(`${res.body.name} is running`) })
+    .catch((fail, res) => toastr.error(res.text))
+  }
+
+  renderButton () {
+    const { installed, name } = this.props
+    return installed ? null
+    : <a href='javascript:void(0)' onClick={this.install.bind(this)} className='install btn btn-filled btn-info'> Install </a>
+  }
+
+  render () {
+    if (this.state.hidden) return null
+
+    const { name, author } = this.props
+    const { netbeast, logo } = this.state
+    const isPlugin = netbeast && (netbeast.type === 'plugin')
+    const defaultLogo = isPlugin ? 'url(/img/plugin.png)' : 'url(/img/dflt.png)'
+    const logoStyle = { backgroundImage: logo ? `url(/api/apps/${name}/logo)` : defaultLogo }
+
+    return (
+      <div className='app'>
+        <div className='logo' title='Launch app' style={logoStyle} onClick={this.launch.bind(this)}>
+        </div>
+        {this.renderButton()}
+        <h4 className='name'>{name}</h4>
+      </div>
+    )
+  }
+}
+
+ExplorableApp.contextTypes = {
+  router: React.PropTypes.object.isRequired
+}

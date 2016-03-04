807fe65363cb68085d9968142de23e3fc58019e8
jsdario <jesus@netbeast.co>
2016-03-04 17:11:48 +0100

    General UX improve new iconset and context menus

--- public/components/apps/app.jsx
+++ public/components/apps/app.jsx
@@ -1,16 +1,30 @@
 /* global toastr */
 import React from 'react'
+import mqtt from 'mqtt'
 import request from 'superagent-bluebird-promise'
+import { OverlayTrigger, Popover } from 'react-bootstrap'
+
+import Pulse from '../misc/activity-pulse.jsx'
 
 export default class App extends React.Component {
   constructor (props, context) {
     super(props)
+    this.state = { isRunning: false, menuHidden: true }
     this.router = context.router
   }
 
   handleClick () {
     const { type } = this.props
     if (type !== 'explore') this.launch()
+  }
+
+  toggleMenu (event) {
+    if (event) event.preventDefault() && event.stopPropagation()
+
+    const { menuHidden } = this.state
+    if (menuHidden) this.refs.contextMenu.show()
+    else this.refs.contextMenu.hide()
+    this.setState({ menuHidden: !menuHidden })
   }
 
   launch () {
@@ -21,7 +35,10 @@ export default class App extends React.Component {
     }).then(() => {
       this.router.push('/i/' + name)
     }).catch((err) => {
-      if (err.status === 404) return toastr.info(`${name} is running`)
+      if (err.status === 404) {
+        this.setState({ isRunning: true })
+        return toastr.info(`${name} is running`)
+      }
       toastr.error(err.message)
     })
   }
@@ -43,42 +60,72 @@ export default class App extends React.Component {
   }
 
   stop () {
-    const { name, dismiss } = this.props
+    const { name, kind, dismiss } = this.props
     request.del('/api/activities/' + name).end((err, res) => {
-      if (err) return toastr.error(res.text)
-      dismiss(name)
+      if (err) return
+
+      this.setState({ isRunning: false })
       toastr.info(name + ' has been stopped.')
+      if (kind !== 'apps' && kind !== 'plugins') dismiss(name)
     })
   }
 
   uninstall () {
-    const { name, dismiss } = this.props
+    const { name, kind, dismiss } = this.props
     request.del('/api/apps/' + name).end((err, res) => {
-      if (err) return toastr.error(res.text)
+      if (err) return
       dismiss(name)
       toastr.info(name + ' has been removed.')
     })
   }
 
+  contextMenu () {
+    const { name } = this.props
+    return (
+      <Popover id={name} className='context-menu'>
+        <a href='javascript:void(0)' onClick={this.stop.bind(this)} className='stop btn btn-filled btn-warning'> Stop </a>
+        <a href='javascript:void(0)' onClick={this.uninstall.bind(this)} className='remove btn btn-filled btn-primary'> Remove </a>
+      </Popover>
+    )
+  }
+
   renderStopButton () {
-    const { type } = this.props
-    return type === 'activities'
+    const { kind } = this.props
+    return kind === 'activities'
     ? <a href='javascript:void(0)' onClick={this.stop.bind(this)} className='stop btn btn-filled btn-warning'> Stop </a>
     : null
   }
 
   renderRemoveButton () {
-    const { type } = this.props
-    return type === 'uninstall'
+    const { kind } = this.props
+    console.log(kind)
+    return kind === 'remove'
     ? <a href='javascript:void(0)' onClick={this.uninstall.bind(this)} className='remove btn btn-filled btn-primary'> Remove </a>
     : null
   }
 
   renderInstallButton () {
-    const { type } = this.props
-    return type === 'explore'
+    const { kind } = this.props
+    return kind === 'explore'
     ? <a href='javascript:void(0)' onClick={this.install.bind(this)} className='install btn btn-filled btn-info'> Install </a>
     : null
+  }
+
+  componentDidMount () {
+    const { name } = this.props
+    this.mqtt = mqtt.connect()
+    this.mqtt.subscribe('netbeast/activities/close')
+    this.mqtt.on('message', function (topic, message) {
+      if (message.toString() === name) this.setState({ isRunning: false })
+    })
+
+    request.get('/api/activities/' + name).end((err, res) => {
+      if (!err) this.setState({ isRunning: true })
+    })
+  }
+
+  componentWillUnmount () {
+    this.mqtt.end()
   }
 
   render () {
@@ -89,9 +136,10 @@ export default class App extends React.Component {
 
     return (
       <div className='app'>
-        <div className='logo' title='Launch app' style={logoStyle}
-        onClick={this.handleClick.bind(this)}>
-        </div>
+        {this.state.isRunning ? <Pulse {...this.props} /> : null}
+        <OverlayTrigger ref='contextMenu' trigger={[]} rootClose placement='bottom' overlay={this.contextMenu()}>
+          <div className='logo' title='Launch app' style={logoStyle} onClick={this.handleClick.bind(this)} onContextMenu={this.toggleMenu.bind(this)} />
+        </OverlayTrigger>
         {this.renderStopButton()}
         {this.renderRemoveButton()}
         {this.renderInstallButton()}

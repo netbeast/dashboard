6b9311c9fbd3caa1f6afd193dbc6d517ccd411b7
franrios <fcojriosbello@gmail.com>
2016-03-28 18:41:14 +0200

    iamalive automated with spawn

--- bin/iamalive-c.js
+++ bin/iamalive-c.js
@@ -10,7 +10,6 @@ var intervalID = setInterval(function () {
   require('getmac').getMac(function (err, macAddress) {
     if (err) throw err
     var msg = new Buffer(macAddress)
-    console.log(process.env.IAMALIVE_SPORT + ' SPORT: ' + process.env.IAMALIVE_CPORT)
     client.send(msg, 0, msg.length, process.env.IAMALIVE_SPORT, process.env.SERVER_IP, function (err) {
       if (err) {
         throw err

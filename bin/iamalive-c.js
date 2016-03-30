30ef831cc4ad1b16717aeeccbd8db7cb330d7763
franrios <fcojriosbello@gmail.com>
2016-03-30 19:52:20 +0200

    Added tunnel as a child process

--- bin/iamalive-c.js
+++ bin/iamalive-c.js
@@ -2,11 +2,21 @@
 
 var dgram = require('dgram')
 var client = dgram.createSocket('udp4')
-var tunnel = require('./tunnel-c.js')
+// var tunnel = require('./tunnel-c.js')
+var path = require('path')
+var spawn = require('child_process').spawn
+
+var env_tunnel = Object.create(process.env)
+env_tunnel
+  .NETBEAST_PORT = process.env.PORT
+  .SERVER_IP = process.env.SERVER_IP
+
+const DASHBOARD_TUNNEL = path.join(__dirname, './tunnel-c.js')
+var tunnelOptions = { env: env_tunnel }
 
 console.log('Server ' + process.env.SERVER_IP + ':' + process.env.IAMALIVE_SPORT)
 
-var intervalID = setInterval(function () {
+setInterval(function () {
   require('getmac').getMac(function (err, macAddress) {
     if (err) throw err
     var msg = new Buffer(macAddress)
@@ -14,6 +24,7 @@ var intervalID = setInterval(function () {
       if (err) {
         throw err
       }
+      console.log('Sendend:' + process.env.IAMALIVE_SPORT + ' to ' + process.env.SERVER_IP)
       // client.close()
     })
   })
@@ -24,13 +35,20 @@ client.on('message', function (msg, rinfo) {
   console.log('client got: ' + remotePort + ' from ' + rinfo.address + ':' + rinfo.port)
   if (parseInt(msg, 10) > 0) {
     console.log()
-    tunnel.start(remotePort)
+    // tunnel.start(remotePort)
+   env_tunnel.RELAY_PORT = remotePort
+   var tunnelOptions = { env: env_tunnel }
+  var tunnel = spawn(DASHBOARD_TUNNEL, tunnelOptions)
   }
+})
+
+process.on('exit', function () {
+  tunnel.kill('SIGTERM')
 })
 
 client.on('listening', function () {
   var address = client.address()
-  console.log('client listening' + address.address + ':' + address.port)
+  console.log('client listening ' + address.address + ':' + address.port)
 })
 
 client.bind(process.env.IAMALIVE_CPORT)

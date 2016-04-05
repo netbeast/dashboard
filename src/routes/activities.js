aa741b53661979362a63a7c89464f9ead3835612
jsdario <jesus@netbeast.co>
2016-04-05 14:40:57 +0200

    Fixes and refactors slightly activity model

--- src/routes/activities.js
+++ src/routes/activities.js
@@ -1,6 +1,6 @@
 var Activity = require('../models/activity')
 var express = require('express')
-var router = express.Router()
+var router = module.exports = express.Router()
 
 // Activities
 // ==========
@@ -12,8 +12,22 @@ router.get('/activities', function (req, res, next) {
 })
 
 router.route('/activities/:name')
-.delete(Activity.close)
-.post(Activity.start)
 .get(Activity.status)
 
-module.exports = router
+.delete(function (req, res, next) {
+  Activity.stop(req.params.name, function (err) {
+    if (err) return next(err)
+    res.status(204).end()
+  })
+})
+
+.post(function (req, res, next) {
+  Activity.boot(req.params.name, function (err, child) {
+    if (err) return next(err)
+    Activity.ready(child, function (err, act) {
+      if (err) return next(err)
+
+      res.json({ name: act.name, port: act.port })
+    })
+  })
+})

3aba578002ce4d99e3a39af9e819c81778d53f0e
jsdario <jesus@netbeast.co>
2016-04-22 15:58:22 +0200

    Modified slightly basic plugin

--- bin/bundles/base_plugin/index.js
+++ bin/bundles/base_plugin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+var netbeast = require('netbeast')
 var express = require('express')
 var cmd = require('commander')
 
@@ -9,6 +10,17 @@ cmd
 .parse(process.argv)
 
 var app = express()
+
+/*
+* Create here your API routes
+* app.get(...), app.post(...), app.put(...), app.delete(...)
+*/
+
+/*
+* Discover your resources / scan the network 
+* And declare your routes into the API
+*/
+
 var server = app.listen(cmd.port || 4000, function () {
   console.log('Netbeast plugin started on %s:%s',
   server.address().address,

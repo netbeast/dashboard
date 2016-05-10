bd55e1da7b632dffa813c24ad2d256545d926bc8
pablopi <pablopizarroaguilar@gmail.com>
2016-05-10 15:18:30 +0200

    create route /i/create

--- src/routes/device.js
+++ src/routes/device.js
@@ -3,9 +3,11 @@
 // modules
 var express = require('express')
 var Promise = require('bluebird')
+var netbeast = require('netbeast')
 
 // librerias propias
 var Resource = require('../models/resource')
+var ApiError = require('../util/api-error')
 var request = require('superagent-bluebird-promise')
 
 var	router = module.exports = express.Router()
@@ -51,4 +53,33 @@ router.route('/i/:key/:value')
       res.json(data)
     }).catch(next)
   })
+})
+
+router.route('/i/create')
+.post(function (req, res, next) {
+  if (!req.body.hook || !req.body.topic || !req.body.app) return res.json(new ApiError(404))
+  else {
+    Resource.find({app: req.body.app, topic: req.body.topic}, function (err, resources) {
+      if (err) return next(err)
+
+      var objects = []
+      if (resources.length > 0) {
+        resources.forEach(function (device) {
+          if (objects.indexOf(device.hook) < 0) objects.push(device.hook)
+        })
+      }
+
+      req.body.hook.forEach(function (id) {
+        var indx = objects.indexOf(id)
+        if (indx >= 0) objects.splice(indx, 1)
+        else netbeast(req.body.topic).create({app: req.body.app, hook: id})
+      })
+
+      if (objects.length > 0) {
+        objects.forEach(function (hooks) {
+          netbeast().delete({ hook: hooks })
+        })
+      }
+    })
+  }
 })

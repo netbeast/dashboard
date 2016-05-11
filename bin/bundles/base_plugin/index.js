c8422eb9d36cf7b6480eb5a084214680dbea9582
jsdario <jesus@netbeast.co>
2016-05-11 17:19:14 +0200

    Improves base plugin

--- bin/bundles/base_plugin/index.js
+++ bin/bundles/base_plugin/index.js
@@ -2,7 +2,7 @@
 
 var netbeast = require('netbeast')
 var express = require('express')
-var cmd = require('commander')
+var cmd = require('commander') // reads --port from command line
 
 cmd
 .version('0.1.42')
@@ -16,10 +16,28 @@ var app = express()
 * app.get(...), app.post(...), app.put(...), app.delete(...)
 */
 
-/*
-* Discover your resources / scan the network 
-* And declare your routes into the API
-*/
+app.get('/:device_id', function (req, res) {
+	// id of the device the dashboard wants
+	// << req.params.device_id >>
+	// dashboard will do GET on this route when
+	// netbeast('topic').get({})
+
+	/* TODO: Return device values from req.query */
+
+	// res.json({ YOUR_PLUGIN_DATA })
+})
+
+app.post('/:device_id', function (req, res) {
+	// id of the device the dashboard wants
+	// << req.params.device_id >>
+	// dashboard will do POST on this route when
+	// netbeast('topic').set({})
+
+	/* TODO: Change device values from req.body */
+
+	// res.json({ YOUR_PLUGIN_DATA })
+})
+
 
 var server = app.listen(cmd.port || 4000, function () {
   console.log('Netbeast plugin started on %s:%s',

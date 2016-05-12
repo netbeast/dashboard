a276dda11f81a14b644ff2281b8cf962c8e92a20
jsdario <jesus@netbeast.co>
2016-05-12 11:21:10 +0200

    Ignores npm-debug.log and refactors api routes tests

--- test/api/routes.js
+++ test/api/routes.js
@@ -10,18 +10,19 @@ const URL = 'https://localhost:' + process.env.SECURE_PORT + '/api'
 
 describe('RESTful Resources API', function () {
   it('should insert a new action in db', function (done) {
-    var req = request.post(URL + '/resources')
-    req.send({ app: 'app', location: 'location', topic: 'topic', groupname: 'group', hook: 'hook' })
-      .end(function (err, resp, body) {
-        should.not.exist(err)
-        resp.statusCode.should.equal(200)
-        done()
-      })
+    request.post(URL + '/resources')
+    .send({ app: 'app', location: 'location', topic: 'topic', groupname: 'group', hook: 'hook' })
+    .end(function (err, resp, body) {
+      should.not.exist(err)
+      resp.statusCode.should.equal(200)
+      done()
+    })
   })
 
   it('should return all specified actions from db', function (done) {
-    var q = 'app=app&topic=topic'
-    request.get(URL + '/resources?' + q).end(function (err, resp, body) {
+    request.get(URL + '/resources')
+    .query({ app: 'app', topic: 'topic' })
+    .end(function (err, resp, body) {
       should.not.exist(err)
       resp.statusCode.should.equal(200)
       body = resp.body
@@ -34,20 +35,21 @@ describe('RESTful Resources API', function () {
   })
 
   it('should update the specified action from db', function (done) {
-    var q = 'app=app&topic=topic'
-    var req = request.patch(URL + '/resources?' + q)
-    req.send({app: 'app2'})
-      .end(function (err, resp, body) {
-        should.not.exist(err)
-        resp.statusCode.should.equal(204)
-        expect(body).to.be.empty
-        done()
-      })
+    request.patch(URL + '/resources')
+    .query({ app: 'app', topic: 'topic' })
+    .send({app: 'app2'})
+    .end(function (err, resp, body) {
+      should.not.exist(err)
+      resp.statusCode.should.equal(204)
+      expect(body).to.be.empty
+      done()
+    })
   })
 
   it('should delete the specified action from db', function (done) {
-    var q = 'hook=hook'
-    request.del(URL + '/resources?' + q).end(function (err, resp, body) {
+    request.del(URL + '/resources')
+    .query({ hook: 'hook' })
+    .end(function (err, resp, body) {
       should.not.exist(err)
       resp.statusCode.should.equal(204)
       expect(body).to.be.empty

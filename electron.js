ec3843879df5c8cd32ccd02d7967e073a944d0e0
Luis <luisfpintos@gmail.com>
2016-06-08 18:14:54 +0200

    Added electron to be unified with master

---
+++ electron.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+var fork = require('child_process').fork
+var path = require('path')
+//Electron
+var mainWindow = null
+var mainURL = null
+const electron = require('electron')
+const elecApp = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const indexJs = path.join(__dirname, './index.js')
+var dashboard = fork(indexJs)
+
+elecApp.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 1000, height: 800, title: 'Netbeast Dashboard | Loading'})
+  mainURL = path.join('file://' + __dirname + '/desktop_app/loading/loading.html')
+  mainWindow.loadURL(mainURL)
+  mainWindow.on('closed', function() {
+      mainWindow = null;
+  })
+})
+
+elecApp.on('activate', function () {
+    if (mainWindow === null) {
+      mainWindow = new BrowserWindow({width: 1000, height: 800, title: 'Netbeast Dashboard'})
+      mainWindow.loadURL(mainURL)
+    }
+})
+
+elecApp.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+     elecApp.quit()
+    }
+    mainWindow = null;
+})
+
+dashboard.on('message', function(data) {
+   console.log(data)
+   mainURL = 'http://localhost:8000'
+   mainWindow.loadURL(mainURL)
+})
+
+process.on('exit', function () {
+	dashboard.kill()
+})
